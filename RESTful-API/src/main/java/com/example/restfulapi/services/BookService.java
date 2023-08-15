4f2a9f44d093e9bfda053fd0f7aeac53e2127339
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-08-15 17:02:25 +0300

    add

--- RESTful-API/src/main/java/com/example/restfulapi/services/BookService.java
+++ RESTful-API/src/main/java/com/example/restfulapi/services/BookService.java
@@ -1,7 +1,39 @@
 package com.example.restfulapi.services;
 
+import com.example.restfulapi.models.dtos.AuthorDTO;
+import com.example.restfulapi.models.dtos.BookDTO;
+import com.example.restfulapi.models.entities.Book;
+import com.example.restfulapi.repositories.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Optional<BookDTO> getBookById(Long id) {
+        return bookRepository.findById(id).map(this::map);
+    }
+
+    public List<BookDTO> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(this::map)
+                .collect(Collectors.toList());
+    }
+
+    private BookDTO map(Book book) {
+        return new BookDTO()
+                .setId(book.getId())
+                .setTitle(book.getTitle())
+                .setIsbn(book.getIsbn())
+                .setAuthor(new AuthorDTO().setName(book.getAuthor().getName()));
+    }
 }
--- RESTful-API/src/main/java/com/example/restfulapi/web/BooksController.java
+++ RESTful-API/src/main/java/com/example/restfulapi/web/BooksController.java
@@ -1,7 +1,43 @@
 package com.example.restfulapi.web;
 
+import com.example.restfulapi.models.dtos.BookDTO;
+import com.example.restfulapi.models.entities.Book;
+import com.example.restfulapi.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api/books")
 public class BooksController {
+    private final BookService bookService;
+
+    @Autowired
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable("id") Long bookId) {
+        Optional<BookDTO> book = bookService.getBookById(bookId);
+
+        if (book.isEmpty()) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        return ResponseEntity.ok(book.get());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
 }
